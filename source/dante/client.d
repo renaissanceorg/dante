34c91c2e01e982a3b96b0440bba6f80c771f3f9f
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-05-03 17:56:43 +0200

    DanteClient
    - Added a few TODOs
    - Instantiate a tristanable `Manager` upon construction
    - Implemented `start()` which starts the `manager`

--- source/dante/client.d
+++ source/dante/client.d
@@ -29,6 +29,9 @@ public class DanteClient
         this(resolvedAddress);
     }
 
+    // TODO: Are we sure we want to do this?
+    // as this connects immediately, then again all of these do
+    // actually (the stream based one assumes an opened stream is passed in)
     this(Address endpointAddress)
     {
         Socket clientSocket = new Socket(endpointAddress.addressFamily(), SocketType.STREAM);
@@ -46,5 +49,27 @@ public class DanteClient
     this(Stream connection)
     {
         this.connection = connection;
+
+        /* Create a tristanable manager based on this */
+        this.manager = new Manager(connection);
     }
+
+    public void start()
+    {
+        // TODO: Change this, just for testing
+
+        // TODO: We need to make unique queues each time we send a message
+        // ... it is here we will find out what the tasky library _actually_
+        // ... needs to do and to be
+        Queue myQueue = new Queue(69);
+        manager.registerQueue(myQueue);
+
+        
+        manager.start();
+    }
+}
+
+unittest
+{
+    DanteClient client = new DanteClient(new UnixAddress("/tmp/renaissance2.sock"));
 }
