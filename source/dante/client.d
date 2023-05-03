ca2f0c12acd2432d676625445cf12c760299c310
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-05-03 15:30:19 +0200

    DanteClient
    - Added a constructor which consumes a river-based `Stream`
    - Added a constructor which takes in an `Address` and opens a `Socket` to it, connects and then passes it wrapped in a river-based `SockStream` to the `Stream`-based cinstructor
    - Added a constructor which takes in `(string domain, ushort port)`, which resolves the domain then passes said address to the `Address`-based constructor (ditto from here)

--- source/dante/client.d
+++ source/dante/client.d
@@ -1,9 +1,50 @@
 module dante.client;
 
+import std.socket;
+import river.core;
+import river.impls.sock : SockStream;
+import davinci;
+
+import tristanable;
+
 public class DanteClient
 {
-    this()
+    /** 
+     * The underlying stream connecting us to
+     * the endpoint server
+     */
+    private Stream connection;
+
+    private Manager manager;
+
+    // TODO: We do this because maybe lookup DNS rather than Address and then decice
+    // ... on whetherto make a TLS stream or not
+    this(string domain, ushort port)
     {
-        
+        // TODO: TLS check etc? Then construct a CryptCLient stream ...
+
+        // TODO: Exception handling for DNS resolution
+        // TODO: Preference over which record to use
+        Address resolvedAddress = getAddress(domain, port)[0];
+        this(resolvedAddress);
+    }
+
+    this(Address endpointAddress)
+    {
+        Socket clientSocket = new Socket(endpointAddress.addressFamily(), SocketType.STREAM);
+        // TODO: Exception handling (for the `connect()` call)
+        clientSocket.connect(endpointAddress);
+        this(new SockStream(clientSocket));
+    }
+
+    /** 
+     * Creates a new Dante client based on the provided stream
+     *
+     * Params:
+     *   connection = the stream to the server
+     */
+    this(Stream connection)
+    {
+        this.connection = connection;
     }
 }
