9600aecb07522cf1a7c8948f50a661e8362abc94
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-05-06 00:04:16 +0200

    - Fixed build issues

--- source/dante/types/nop.d
+++ source/dante/types/nop.d
@@ -7,7 +7,7 @@ public class NopRequest : Request
     this()
     {
         import davinci.c2s.test;
-        import davinci.base.base;
+        import davinci;
         TestMessage testMessage = new TestMessage();
         super(new BaseMessage(MessageType.CLIENT_TO_SERVER, CommandType.NOP_COMMAND, testMessage).encode());
     }
